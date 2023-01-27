11124e0d94a6a685ece34925480a91776927dd5c
Rapeepan Moonthai <rapeeza1598@gmail.com>
2023-01-27 09:26:23 +0700

    Add Update ID

--- mvc/view.php
+++ mvc/view.php
@@ -33,10 +33,11 @@
             foreach ($book as $col) {
                 echo "<td>{$col}</td>";
             }
-            echo "<td><a href = 'control.php?id={book['ISBN']}'>Edit</a> | <a href = 'control.php?id={book['ISBN']}'>Del</a></td></tr>";
+            echo "<td><a href = 'control.php?updateid={book['ISBN']}'>Edit</a> | <a href = 'control.php?id={book['ISBN']}'>Del</a></td></tr>";
         }
         ?>
     </table>
+    <?php if(isset($_GET["update"])){ ?>
     <div>
         <form action="./control.php" method="post">
             <h3>รหัสหนังสือ</h3>
@@ -59,6 +60,30 @@
             <button type="submit" name="save">เพืิ่มหนังสือ</button>
         </form>
     </div>
+    <h1>Edit</h1>
+    <div>
+        <form action="./control.php" method="post">
+            <h3>รหัสหนังสือ</h3>
+            <input type="text" name="ISBN" id="" placeholder="รหัสหนังสือ" require>
+            <h3>ชื่อหนังสือ</h3>
+            <input type="text" name="bookname" id="" placeholder="รหัสหนังสือ" require>
+            <h3>ประเภทหนังสือ</h3>
+            <select name="Type" id="" require>
+                <?php
+                $option = ["บันเทิง", "วิทยาศาสตร์", "บันเทิง", "วิชาการ", "อื่นๆ"];
+                foreach ($option as $opt) {
+                ?>
+                    <option value="<?= $opt; ?>"><?= ($opt == $book_data["BookType"]?"selected":""); ?></option>
+                <?php } ?>
+            </select>
+            <h3>ราคานำหน่าย</h3>
+            <input type="text" name="Price" id="" placeholder="ราคา" require>
+            <h3>จำนวน</h3>
+            <input type="text" name="Qty" id="" placeholder="จำนวน(เล่ม)" require>
+            <button type="submit" name="save">เพืิ่มหนังสือ</button>
+        </form>
+    </div>
+    <?php } ?>
 </body>
 
 </html>
