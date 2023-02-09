6826856e27f06079cc890123e58a82aa412d925e
Rapeepan Moonthai <rapeeza1598@gmail.com>
2023-02-09 14:04:22 +0700

    api get

---
+++ restful/phpget.php
@@ -0,0 +1,4 @@
+<?php
+if(isset($_GET["data1"])&&isset($_GET["data2"])){
+    echo "Your input:<u>{$_GET["data1"]}</u> and <u>{$_GET["data2"]}</u>";
+}
---
+++ restful/script.js
@@ -0,0 +1,23 @@
+async function exec() {
+    let d1 = document.getElementById("txt1").value
+    let d2 = document.getElementById("txt2").value
+
+    let url =
+        "http://localhost:3000/restful/phpget.php?data1=" + d1 + "&data2=" + d2
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+        if (!response.ok) {
+            const message = "Error with Status code {response.status}"
+            throw new Error(message)
+        }
+        const data = await response.text()
+        document.getElementById("div1").innerHTML = `<strong>${data}</strong>`
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
