5028be9c74bce6998247e045d57c3b61a72a2d7c
Rapeepan Moonthai <rapeeza1598@gmail.com>
2023-02-09 14:40:48 +0700

    add method

--- restful/phpget.php
+++ restful/phpget.php
@@ -2,3 +2,31 @@
 if(isset($_GET["data1"])&&isset($_GET["data2"])){
     echo "Your input:<u>{$_GET["data1"]}</u> and <u>{$_GET["data2"]}</u>";
 }
+?>
+<?php
+$method = $_SERVER["REQUEST_METHOD"];
+switch($method){
+    case 'POST':
+        // $data = $_POST;
+        echo "POST";
+        break;
+    case 'GET':
+        // $data = $_GET;
+        echo "GET";
+        break;
+    case 'PUT':
+        // $data = $_PUT;
+        echo "PUT";
+        break;
+    case 'DELETE':
+        // $data = $_DELETE;
+        echo "DELETE";
+        break;
+    case 'NEW':
+        // $data = $_NEW;
+        echo "NEW";
+        break;
+    default:
+        echo "not support {$method} now";
+}
+?></
--- restful/script.js
+++ restful/script.js
@@ -21,3 +21,27 @@ async function exec() {
         console.log(error)
     }
 }
+
+async function exec_opt() {
+    let d1 = document.getElementById("txt1").value
+    let d2 = document.getElementById("txt2").value
+    let getmethod = document
+        .querySelector('input[name="opt1"]:checked')
+        .value.toUpperCase()
+    let url = "http://localhost:3000/restful/phpget.php"
+    try {
+        const response = await fetch(url, {
+            method: getmethod,
+            headers: { "Content-Type": "application/json" },
+        })
+        if (!response.ok) {
+            const message = "Error with Status code {response.status}"
+            throw new Error(message)
+        }
+        const data = await response.text()
+        document.getElementById("div1").innerHTML = `<strong>${data}</strong>`
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
